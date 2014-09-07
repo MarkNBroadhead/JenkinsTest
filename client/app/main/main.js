d8952b500076ce3942857c5edf8cc997f97aa33f
Mark N Broadhead <MarkNBroadhead@gmail.com>
2014-09-06 18:09:55 -0600

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('qNinjaApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
